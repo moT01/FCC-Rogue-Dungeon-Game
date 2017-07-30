f4d3082ed360fd64ddd2380dd01e06b9d080aece
Tom M <moT01@users.noreply.github.com>
2017-07-30 18:44:36 -0500

    Update rogue.js

--- rogue.js
+++ rogue.js
@@ -184,7 +184,7 @@ class Game extends React.Component {
                         tempObj.xpReward = Math.floor(Math.random() * 10 + 20);
                         break;
                     case "king":
-                        tempObj.health = Math.floor(Math.random() * 20 + 180);
+                        tempObj.health = Math.floor(Math.random() * 20 + object.king.health);
                         tempObj.offense = Math.floor(Math.random() * 5 + 15);
                         tempObj.defense = Math.floor(Math.random() * 4 + 12);
                         tempObj.healthReward = Math.floor(Math.random() * 10 + 40);
